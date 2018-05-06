408b579b9b95b0e5175735a3a84dc943d0c685f0
Anup Biswas <anup04sust@gmail.com>
2018-05-06 18:22:54 +0600

    Using compass

---
+++ development/index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html amp lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome – Revamp 4G AMP</title>
+        <link rel="canonical" href="index.php">
+        <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+        <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+        <style amp-custom><?php echo file_get_contents('http://rev4g.me/development/source/css/main.css') ?></style>
+        <script async src="https://cdn.ampproject.org/v0.js"></script>
+        <script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>
+    </head>
+    <body class="lang-en">
+        <header class="ampstart-headerbar">
+            <div role="button" aria-label="open sidebar" on="tap:header-sidebar.toggle" tabindex="0" class="ampstart-navbar-trigger">
+                <span class="toggle-btn">
+                    <span class="btn-bar bar-1"></span>
+                    <span class="btn-bar bar-1"></span>
+                    <span class="btn-bar bar-1"></span>
+                </span>
+            </div>
+
+            <a href="/" class="ampstart-headerbar-home-link home-en">
+                Grameenphone
+            </a>
+            <nav class="ampstart-headerbar-nav ampstart-nav">
+                <ul>
+                    <li class="ampstart-nav-item ">
+                        <a href="#" class="ln-link-bn">বাংলা</a>
+                    </li>
+                    <li class="ampstart-nav-item ">
+                        <a href="#">About 4G</a>
+                    </li>
+                    <li class="ampstart-nav-item ">
+                        <a href="#">4G Coverage</a>
+                    </li>
+                    <li class="ampstart-nav-item ">
+                        <a href="#">FAQ</a>
+                    </li>
+                    <li class="ampstart-nav-item ">
+                        <a href="#">Get a 4g device</a>
+                    </li>
+                    <li class="ampstart-nav-item ">
+                        <a href="#" class="btn-bordered">Get a 4g sim</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <section class="amp-page-banner">
+            <div class="container">
+
+                <div class="banner-content">
+                    <div class="content-inner">
+                        <h1 class="banner-title">Are you 4G ready?</h1>
+                        <p class="banner-desc">
+                            4G will provide higher speeds, improved efficiencies, and better access to the digital world. Upgrade to 4G SIM and get 1.5 GB Internet FREE 
+                        </p>
+                    </div>
+                    <div class="banner-carousel">
+                        <amp-carousel height="100"
+                                      layout="fixed-height"
+                                      type="carousel">
+                            <div class="fourg-steps">
+                                <div class="step-content">
+                                    <span class="icon-inner"></span>
+                                    <span class="icon-sr">Have a 4G SIM</span>
+                                </div>
+                            </div>
+                            <div class="fourg-steps">
+                                <div class="step-content">
+                                    <span class="icon-inner"></span>
+                                    <span class="icon-sr">Have a 4G SIM</span>
+                                </div>
+                            </div>
+                            <div class="fourg-steps">
+                                <div class="step-content">
+                                    <span class="icon-inner"></span>
+                                    <span class="icon-sr">Have a 4G SIM</span>
+                                </div>
+                            </div>
+                            <div class="fourg-steps">                                
+                                <div class="step-content">
+                                    <span class="icon-inner"></span>
+                                    <span class="icon-sr">Have a 4G SIM</span>
+                                </div></div>
+                        </amp-carousel>
+                    </div>
+                </div>
+                <div class="banner-animation">
+                    <amp-img src="source/images/4g-light.png" alt="Welcome" height="358" width="470"></amp-img>
+                </div>
+            </div>
+            <div class="container-tes"></div>
+        </section>
+        <footer>Footer</footer>
+    </body>
+</html>
---
+++ development/source/config.rb
@@ -0,0 +1,39 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = false
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+environment = :production
+
+# You can select your preferred output style here (can be overridden via the
+# command line)
+#output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# Pass options to sass.
+# - For development, we turn on the FireSass-compatible debug_info.
+# - For production, we force the CSS to be regenerated even though the source
+#   scss may not have changed, since we want the CSS to be compressed and have
+#   the debug info removed.
+sass_options = (environment == :development) ? {:debug_info => false} : {:always_update => true}
